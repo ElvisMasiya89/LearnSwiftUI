7130bef431ee12ab06092c21dc6ddd13a25234e5
ElvisM89 <80644724+ElvisMasiya89@users.noreply.github.com>
2022-09-01 00:05:52 +0200

    MVVM
    MVVM

---
+++ mvvm-demo/mvvm-demo/Models/Recipe.swift
@@ -0,0 +1,13 @@
+//
+//  Recipe.swift
+//  mvvm-demo
+//
+//  Created by Elvis Masiya on 2022/09/01.
+//
+
+import Foundation
+
+struct Recipe{
+    var name = ""
+    var cuisine = ""
+}
---
+++ mvvm-demo/mvvm-demo/mvvm_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvm_demoApp.swift
+//  mvvm-demo
+//
+//  Created by Elvis Masiya on 2022/08/31.
+//
+
+import SwiftUI
+
+@main
+struct mvvm_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

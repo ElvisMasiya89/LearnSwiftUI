89d114c310b07d4405a35be6f908136ae7dd1fe5
ElvisM89 <80644724+ElvisMasiya89@users.noreply.github.com>
2022-09-04 01:33:50 +0200

    RecipeApp Navigation
    RecipeApp Navigation

---
+++ RecipeApp/RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Elvis Masiya on 2022/09/03.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipeListView()
+        }
+    }
+}
---
+++ RecipeApp/RecipeApp/Views/RecipeListView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  RecipeApp
+//
+//  Created by Elvis Masiya on 2022/09/03.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    
+    //Reference the View Model
+    @ObservedObject var model = RecipeModel()
+    
+    var body: some View {
+        NavigationView {
+            List(model.recipes){ recipe in
+                
+                NavigationLink {
+                    RecipeDetailsView(recipe: recipe)
+                } label: {
+                    HStack(alignment: .center, spacing: 20.0) {
+                        Image(recipe.image)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 50, height: 50, alignment: .center)
+                            .clipped()
+                            .cornerRadius(5)
+                        Text(recipe.name)
+                    }
+                }
+                
+            }.navigationBarTitle("All Recipes")
+        }
+    }
+}
+
+struct RecipeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeListView()
+    }
+}

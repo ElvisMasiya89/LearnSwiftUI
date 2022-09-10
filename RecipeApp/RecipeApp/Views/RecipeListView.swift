659b162f3bedd1a55ceb962839817ff9d9246b4f
ElvisM89 <80644724+ElvisMasiya89@users.noreply.github.com>
2022-09-10 13:19:25 +0200

    LazyStacks
    LazyStacks

--- RecipeApp/RecipeApp/Views/RecipeListView.swift
+++ RecipeApp/RecipeApp/Views/RecipeListView.swift
@@ -9,36 +9,47 @@ import SwiftUI
 
 struct RecipeListView: View {
     
-//    //Reference the View Model
-//    @ObservedObject var model = RecipeModel()
+    //    //Reference the View Model
+    //    @ObservedObject var model = RecipeModel()
     
     @EnvironmentObject var model:RecipeModel
-
+    
     var body: some View {
         NavigationView {
-            List(model.recipes){ recipe in
+            VStack(alignment: .leading){
                 
-                NavigationLink {
-                    RecipeDetailsView(recipe: recipe)
-                } label: {
-                    HStack(alignment: .center, spacing: 20.0) {
-                        Image(recipe.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 50, height: 50, alignment: .center)
-                            .clipped()
-                            .cornerRadius(5)
-                        Text(recipe.name)
+                Text("All Recipes").bold()
+                    .padding(.top, 40)
+                    .font(.largeTitle)
+                
+                ScrollView {
+                    LazyVStack(alignment: .leading){
+                        ForEach(model.recipes){ recipe in
+                            
+                            NavigationLink {
+                                RecipeDetailsView(recipe: recipe)
+                            } label: {
+                                HStack(alignment: .center, spacing: 20.0) {
+                                    Image(recipe.image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 50, height: 50, alignment: .center)
+                                        .clipped()
+                                        .cornerRadius(5)
+                                    Text(recipe.name).foregroundColor(.black)
+                                }
+                            }
+                            
+                        }.navigationBarHidden(true)
                     }
                 }
-                
-            }.navigationBarTitle("All Recipes")
+            }.padding(.leading)
         }
     }
 }
 
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeListView()
+        RecipeListView().environmentObject(RecipeModel())
     }
 }
